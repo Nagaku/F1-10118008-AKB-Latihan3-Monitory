c787de92c157b5f54fef21bb5e7fd63295a9f3ac
Nagaku <mimelnagaku1@gmail.com>
2021-05-08 13:55:08 +0700

    Final Commit

--- app/src/main/java/com/example/monitory/FinalActivity.java
+++ app/src/main/java/com/example/monitory/FinalActivity.java
@@ -1,4 +1,20 @@
 package com.example.monitory;
 
-public class FinalActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout4);
+        Intent intent = getIntent();
+        String text1 = intent.getStringExtra(MainActivity.NAMA);
+        TextView textView = findViewById(R.id.layout4_text1);
+        textView.setText("Beres! Sekarang " + text1 + " udah bisa ngecek penggunaan HP  " + text1 + "  tiap hari buat bantu " + text1 + " ngatur waktu :)");
+    }
 }
--- app/src/main/java/com/example/monitory/MainActivity.java
+++ app/src/main/java/com/example/monitory/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.monitory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NAMA = "com.example.monitory.nama";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface tf = ResourcesCompat.getFont(
+                this,
+                R.font.nunito_bold);
+        TextView t1 = findViewById(R.id.layout1_text1);
+        TextView t2 = findViewById(R.id.layout1_text2);
+        t1.setTypeface(tf);
+        t2.setTypeface(tf);
+
+
+    }
+
+    public void layout1(View view){
+        setContentView(R.layout.activity_layout2);
+    }
+
+
+    public void layout2(View view){
+        setContentView(R.layout.activity_layout3);
+
+    }
+
+
+    public void layout3(View view){
+        Intent intent = new Intent(this, FinalActivity.class);
+        EditText et = (EditText) findViewById(R.id.layout3_text2);
+        String nama = et.getText().toString();
+        intent.putExtra(NAMA, nama);
+        startActivity(intent);
     }
 }
